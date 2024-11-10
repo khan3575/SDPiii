9a3ae93016d0e87d5d8aa36094ac38e8ae882397
Sakib Khan <114014409+khan3575@users.noreply.github.com>
2024-11-10 23:22:41 +0600

    -registration databse is working -page not moving correcting

--- sdp_iii/sdpApp/models.py
+++ sdp_iii/sdpApp/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser,BaseUserManager,PermissionsMixin
 
-# Create your models here.
+# Create your models here
+# Create adress table
+class Address(models.Model):
+    country = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    district= models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.country},{self.city},{self.district}"
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)  # Hash the password
+        user.save(using=self._db)
+        return user
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+
+class Users(AbstractBaseUser, PermissionsMixin):
+    user_id = models.AutoField(primary_key=True)
+    email = models.EmailField(unique=True, null=False)
+    first_name = models.CharField(max_length=255, null=False)
+    last_name = models.CharField(max_length=255, null=False)
+    address = models.ForeignKey(Address, on_delete=models.SET_NULL, null=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'  # Email is used as the unique identifier
+    REQUIRED_FIELDS = ['first_name', 'last_name']  # These fields are required when creating a superuser
+
+    def __str__(self):
+        return self.email
